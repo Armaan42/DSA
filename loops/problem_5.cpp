33eda2c2bc3713e656439f2636d37748e809c3a4
Armaan <armaansinghbhau8@gmail.com>
2026-03-25 21:55:10 +0530

    WAP to count digits of a given numbers

--- loops/problem_5.cpp
+++ loops/problem_5.cpp
@@ -7,12 +7,19 @@ int main(){
     int n;
     cin >> n;
 
+    if(n == 0){
+        cout << 1 << endl;
+        return 0;
+    }
+
     int count = 0;
 
-    while(n>0){
-        n = n/10;
+    while(n > 0){
+        n = n / 10;
         count++;
     }
+    
     cout << count << endl;
+
     return 0;
 }
