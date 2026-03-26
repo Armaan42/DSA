4375d6534b99f54a0421d8b0dbe152a54f139a55
Armaan <armaansinghbhau8@gmail.com>
2026-03-26 21:04:12 +0530

    print the nth fibonacci number

---
+++ loops/problem_9.cpp
@@ -0,0 +1,19 @@
+// print the nth fibonacci number
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+
+    int a = 0;
+    int b = 1;
+
+    for(int i=1; i<=n; i++){
+        int c = a + b;
+        a = b;
+        b = c;
+    }
+    cout << b << endl;
+    return 0;
+}
