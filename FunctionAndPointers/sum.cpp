b6aca79e02d57fa62bc79bff64180c0b94c2e51e
Armaan <armaansinghbhau8@gmail.com>
2026-03-28 23:29:29 +0530

    learning function

---
+++ FunctionAndPointers/sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int sum(int x, int y){
+    return x + y;
+}
+
+int main(){
+    cout << sum(1,2) << endl;
+    cout << max(1,2) << endl;
+    cout << min(1,2) << endl;
+    return 0;
+}
