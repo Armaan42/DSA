b200b658757572aabe53855e681c55a725e5b67f
Armaan <armaansinghbhau8@gmail.com>
2026-03-29 18:16:42 +0530

    find the second maximum value out of all elements in the array

---
+++ arrays/problem_2.cpp
@@ -0,0 +1,29 @@
+// find the second maximum value out of all elements in the array
+
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int main(){
+    int arr[] = {100,2,30,4,5};
+
+    int max = INT_MIN;
+    int smax = INT_MIN;
+
+    for(int i=0; i<sizeof(arr)/sizeof(arr[0]); i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+    
+    cout << max << endl;
+
+    for(int i=0; i<sizeof(arr)/sizeof(arr[0]); i++){
+        if(smax < arr[i] && arr[i] != max){
+            smax = arr[i];
+        }
+    }
+
+    cout << smax << endl;
+
+}
