c9a739dc0b0c54387c869fa0d9b53235bfe1be50
Armaan <armaansinghbhau8@gmail.com>
2026-03-31 15:34:33 +0530

    WAP to sort all 0's and 1's

---
+++ vector/problem_6.cpp
@@ -0,0 +1,38 @@
+// WAP to sort all 0's and 1's
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void sort01(vector<int> &v){
+    int n = v.size();
+
+    int noo = 0;
+    int noz = 0;
+
+    for(int i=0; i<v.size(); i++){
+        if(v[i] == 0){
+            noz++;
+        }noo++;
+    }
+
+    for(int i=0; i<v.size(); i++){
+        if(i < noz) v[i] = 0;
+        else v[i] = 1;
+    }
+}
+
+int main(){
+    vector<int> v{0,1,1,1,0,0,1,0,0};
+
+    for(int i=0; i<v.size(); i++){
+        cout << v[i] << " ";
+    }cout << endl;
+
+    sort01(v);
+
+    for(int i=0; i<v.size(); i++){
+        cout << v[i] << " ";
+    }cout << endl;
+
+    return 0;
+}
