19fd253b1009edb73f3270c287d0206868b47dcb
Armaan <armaansinghbhau8@gmail.com>
2026-04-03 20:59:45 +0530

    max in 2D arrays

---
+++ arrays/problem_4.cpp
@@ -0,0 +1,29 @@
+//max in 2D arrays
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int m, n;
+    cin >> m >> n;
+
+    int arr[m][n];
+
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            cin >> arr[i][j];
+        }
+    }
+
+    int max = 0;
+
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            if(arr[i][j] > max){
+                max = arr[i][j];
+            }
+        }
+    }
+    cout << max << endl;
+    return 0;
+}
