ee513c2af66cbb2a1efe32b724f5e44c604315c9
Armaan <armaansinghbhau8@gmail.com>
2026-04-12 21:05:32 +0530

    count the different neighbouring character

---
+++ strings/problem_1.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string s = "abbcdeffghh";
+    int count = 0;
+    int n = s.length();
+
+    for(int i=0; i<s.length(); i++){
+        if(n==1){
+            break;
+        }
+        if(i==0){
+            if(s[i] != s[i+1])count++;
+        }
+        else if(i==n-1){
+            if(s[i] != s[i-1])count++;
+        }
+        else if(s[i] != s[i-1] && s[i] != s[i+1]){
+            count++;
+        }
+    }
+    cout << count << endl; 
+
+    return 0;
+}
