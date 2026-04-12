afa354fe0f41a8fb1902b54e5ba67a403eaef4de
Armaan <armaansinghbhau8@gmail.com>
2026-04-12 20:40:35 +0530

    introduction to strings

---
+++ strings/introStrings.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // char str[] = {'a', 'b', '\0', 'c'};
+
+    // for(int i=0; i<str[i]!='\0'; i++){
+    //     cout << str[i] << " ";
+    // }
+    // cout << str << endl;
+
+    string s;
+
+    getline(cin ,s);
+
+    cout << s;
+
+    cout << endl;
+    return 0;
+}
