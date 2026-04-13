1dc4048e7613decf3d917b345b4bc38a4ce9ba35
Armaan <armaansinghbhau8@gmail.com>
2026-04-13 11:08:01 +0530

    learn concept of stoi and stoll

---
+++ strings/stoi_stoll.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string str = "12456";
+    int x = stoi(str);
+    cout << x << endl;
+    cout << x + 1 << endl;
+
+
+    //stoll
+    
+}
